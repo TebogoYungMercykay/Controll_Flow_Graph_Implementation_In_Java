5331920fbf1ba32b586501398dece0e929f80a13
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-06-06 16:22:55 +0200

    The nodes (code blocks) and the edges in the CFG.

--- Code/Edge.java
+++ Code/Edge.java
@@ -1,33 +1,39 @@
 public class Edge {
+    // This is the class that will represent the edges in the CFG.
     private final String annotation;
     private final Node nextNode;
     private final int computationalTime;
 
     public Edge(String annot, Node nextNode, int compTime){
         //TODO: Implement the function
+        this.annotation = annot;
+        this.nextNode = nextNode;
+        this.computationalTime = compTime;
     }
 
     public Node getNext(){
         //TODO: Implement the function
+        return this.nextNode;
     }
 
     public String getAnnotation(){
         //TODO: Implement the function
+        return this.annotation;
     }
 
     public int getCompTime(){
         //TODO: Implement the function
+        return this.computationalTime;
     }
 
     public String toString(){
-        //Provided function, do not alter!!!
-        String res = "-> " + annotation + " -[" + computationalTime + "]-"; 
-        
-        if(nextNode == null)
+        // Do not alter!!!
+        String res = "-> " + annotation + " -[" + computationalTime + "]-";
+        if(nextNode == null) {
             res += "> NULL";
-        else
+        } else {
             res += "> " + nextNode.getAnnotation();
-
+        }
         return res;
     }
 }
--- Code/Node.java
+++ Code/Node.java
@@ -1,32 +1,39 @@
 public class Node {
+    // This will be the class that represents the nodes (code blocks) in the CFG.
     private final String annotation;
-    private final /*Own data structure goes here of type Edge*/ edges;
-    
+    private final List<Edge> edges;
+
     public Node(String annot){
         //TODO: Implement the function
+        this.annotation = annot;
+        this.edges = new List<>();
     }
 
     public void addEdge(Node nextNode, String annotation, int compTime){
         //TODO: Implement the function
+        edges.append(new Edge(annotation, nextNode, compTime));
     }
 
     public String getAnnotation(){
         //TODO: Implement the function
+        return this.annotation;
     }
 
     public Edge[] getEdges(){
         //TODO: Implement the function
+        return (Edge[]) edges.toArray();
     }
 
     public String toString(){
-        //Provided function, do not alter!!!
+        // Do not alter!!!
         String res = annotation + ":\n";
-        if(edges.toArray().length == 0)
+        if(edges.toArray().length == 0) {
             res += "\t" + "No out going edges" + "\n";
-        else
+        } else {
             for(Object e: edges.toArray()){
                 res += "\t" + e.toString() + "\n";
-            } 
+            }
+        }
         return res;
     }
 }
