d75e2f284c185ac8dcff97fe27ad3222a8401859
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-06-09 15:40:10 +0200

    The Testing Main for the CFG Implementation.

--- Code/App.java
+++ Code/App.java
@@ -5,7 +5,53 @@ public class App {
         // TODO: Something like OwnDataStructure<Edge> or so.
         TestingTheEdgeClass();
         TestingThePathClass();
-        // SpecExample1();
+        SpecExample1();
+    }
+
+    public static void SpecExample1(){
+        System.out.println("________________________ S T A R T _____________________________");
+        Node N1 = new Node("N1");
+        Node N2 = new Node("N2");
+        Node N3 = new Node("N3");
+        Node N4 = new Node("N4");
+        Node N5 = new Node("N5");
+        CFG cfg = new CFG();
+        cfg.addStartNode(N1);
+        cfg.addNode(N2);
+        cfg.addNode(N3);
+        cfg.addNode(N4);
+        cfg.addExitNode(N5);
+        cfg.addEdge("E1", cfg.getNode("N1"), cfg.getNode("N2"), 0);
+        cfg.addEdge("E2", cfg.getNode("N2"), cfg.getNode("N3"), 0);
+        cfg.addEdge("E3", cfg.getNode("N3"), cfg.getNode("N4"), 0);
+        cfg.addEdge("E4", cfg.getNode("N4"), cfg.getNode("N3"), 0);
+        cfg.addEdge("E5", cfg.getNode("N3"), cfg.getNode("N5"), 0);
+        printGraph(cfg);
+        Path[] simplePaths = cfg.getSimplePaths();
+        for(Path p: simplePaths){
+            System.out.println(p.toString());
+        }
+        printBreaker();
+        Path[] primePaths = cfg.getPrimePaths();
+        for(Path p: primePaths){
+            System.out.println(p.toString());
+        }
+        printBreaker();
+        System.out.println("isSESE: " + cfg.isSESE());
+        System.out.println("isValid: " + cfg.isValid());
+        System.out.println("Shortest: " + cfg.shortestCompTimePath(N1, N2).toString());
+        System.out.println("Shortest: " + cfg.shortestCompTimePath(N1, N4).toString());
+        System.out.println("Shortest: " + cfg.shortestCompTimePath(N1, N5).toString());
+        System.out.println("Shortest: " + cfg.shortestCompTimePath(N3, N4).toString());
+        System.out.println("Shortest: " + cfg.shortestCompTimePath(N2, N5).toString());
+        System.out.println("Shortest: " + cfg.shortestCompTimePath(N4, N5).toString());
+        System.out.println("isReachable: " + cfg.isReachable(N2, N5));
+        printBreaker();
+        for(CFG graph : cfg.splitGraph()) {
+            printGraph(graph);
+        }
+        System.out.println("__________________________ E N D _______________________________");
+        System.out.println("________________________________________________________________");
     }
 
     public static void TestOwnDataStructure(){
@@ -153,35 +199,6 @@ public class App {
         System.out.println("________________________________________________________________");
     }
 
-    public static void SpecExample1(){
-        Node N1 = new Node("N1");
-        Node N2 = new Node("N2");
-        Node N3 = new Node("N3");
-        Node N4 = new Node("N4");
-        Node N5 = new Node("N5");
-        CFG cfg = new CFG();
-        cfg.addStartNode(N1);
-        cfg.addNode(N2);
-        cfg.addNode(N3);
-        cfg.addNode(N4);
-        cfg.addExitNode(N5);
-        cfg.addEdge("E1", cfg.getNode("N1"), cfg.getNode("N2"), 0);
-        cfg.addEdge("E2", cfg.getNode("N2"), cfg.getNode("N3"), 0);
-        cfg.addEdge("E3", cfg.getNode("N3"), cfg.getNode("N4"), 0);
-        cfg.addEdge("E4", cfg.getNode("N4"), cfg.getNode("N3"), 0);
-        cfg.addEdge("E5", cfg.getNode("N3"), cfg.getNode("N5"), 0);
-        printGraph(cfg);
-        Path[] simplePaths = cfg.getSimplePaths();
-        for(Path p: simplePaths){
-            System.out.println(p.toString());
-        }
-        printBreaker();
-        Path[] primePaths = cfg.getPrimePaths();
-        for(Path p: primePaths){
-            System.out.println(p.toString());
-        }
-    }
-
     public static void printGraph(CFG graph){
         printBreaker();
         if(graph == null){
@@ -193,9 +210,11 @@ public class App {
     }
 
     public static void printBreaker(){
-        System.out.println("###");
+        System.out.println("________________________________________________________________");
+        System.out.println("________________________________________________________________");
     }
-/* THIS IS THE OUTPUT I GET, IT DOESN'T GUARANTEE FULL MARKS!!!
+/* THIS IS THE OUTPUT FOR THE CFG!!!
+java App
 ________________________ S T A R T _____________________________
 List: 1,2,3,4
 Is empty? false
@@ -245,12 +264,9 @@ To String Path 3: Node 6-> Edge 5 -[67]-> Node 6-> Edge 6 -[98]-> Node 7
 Valid Path 1: true
 __________________________ E N D _______________________________
 ________________________________________________________________
- */
-
-// * The following output is still Raw, Uncut and with No Edits.
-
-/* Expected output
-###
+________________________ S T A R T _____________________________
+________________________________________________________________
+________________________________________________________________
 N1:
         -> E1 -[0]-> N2
 N2:
@@ -263,7 +279,8 @@ N4:
 N5:
         No out going edges
 
-###
+________________________________________________________________
+________________________________________________________________
 N1
 N2
 N3
@@ -288,6 +305,37 @@ N4-> E4 -[0]-> N3-> E3 -[0]-> N4
 N4-> E4 -[0]-> N3-> E5 -[0]-> N5
 N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E3 -[0]-> N4
 N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E5 -[0]-> N5
+________________________________________________________________
+________________________________________________________________
+isSESE: true
+isValid: true
+Shortest: N1-> E1 -[0]-> N2
+Shortest: N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E3 -[0]-> N4
+Shortest: N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E5 -[0]-> N5
+Shortest: N3-> E3 -[0]-> N4
+Shortest: N2-> E2 -[0]-> N3-> E5 -[0]-> N5
+Shortest: N4-> E4 -[0]-> N3-> E5 -[0]-> N5
+isReachable: true
+________________________________________________________________
+________________________________________________________________
+________________________________________________________________
+________________________________________________________________
+N1:
+        -> E1 -[0]-> N2
+N2:
+        -> E2 -[0]-> N3
+N3:
+        -> E3 -[0]-> N4
+        -> E5 -[0]-> N5
+N4:
+        -> E4 -[0]-> N3
+N5:
+        No out going edges
+
+________________________________________________________________
+________________________________________________________________
+__________________________ E N D _______________________________
+________________________________________________________________
  */
 
 }
